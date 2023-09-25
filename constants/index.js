df8a3645d661279552e6575d4c00a09dc0f453be
kali <lozada_paolo@yahoo.com>
2023-09-25 13:59:41 +0800

    solana-wishing-well-092523

---
+++ constants/index.js
@@ -0,0 +1,3 @@
+import { PublicKey } from "@solana/web3.js";
+
+export const TODO_PROGRAM_PUBKEY = new PublicKey("GMuJZXce6AmpkRYmh7nfnWbV5a2tFjGdK8kixzh5hu2t");
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,33 @@
-import '@/styles/globals.css'
+import Head from 'next/head'
+import '../styles/global.css'
+// Import WalletConnectionProvider from components
+import { WalletConnectProvider } from '../components/WalletConnectProvider'
+// Import the solana wallet css
+import '@solana/wallet-adapter-react-ui/styles.css'
+ 
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+    return (
+        <>
+            <div style={{ textAlign: 'center', color: 'white', fontSize: '30px' }}>Solana Blockchain</div>
+            <div style={{ textAlign: 'center', color: 'white', fontSize: '50px' }}>Wish List</div>
+            <div style={{ textAlign: 'center', color: 'white', fontSize: '11px' }}>INSTRUCTIONS: {'\u00A0'} 1. Connect to Phantom Wallet, {'\u00A0'} {'\u00A0'} 2. Click Initialize for New User, {'\u00A0'} {'\u00A0'} 3. Add your Name and Wish and Press Enter, {'\u00A0'} {'\u00A0'} 4. Click the Check Box for Wish Granted, {'\u00A0'} {'\u00A0'} 5. Click the Trash Icon if you want to delete a Wish Granted </div>
+
+            <main>
+                {/* Wrap provider around App */}
+                <WalletConnectProvider>
+                    <Component {...pageProps} />
+                </WalletConnectProvider>
+            </main>
+           
+        </>
+    )
 }
+
+export default MyApp
+
+
+
+
+
+
